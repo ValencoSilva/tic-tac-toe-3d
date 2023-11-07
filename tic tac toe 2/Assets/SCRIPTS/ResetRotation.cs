16e6536271df62c432ba70025efd2c81c70dd0c7
Filipe Barcellos <filipe.barcellos@ufrj.br>
2023-11-06 23:43:09 -0300

    upd camera e reset

---
+++ tic tac toe 2/Assets/SCRIPTS/ResetRotation.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ResetRotation : MonoBehaviour
+{
+    Vector3 originalPos;
+    Vector3 originalRot;
+    Quaternion startRotation;
+    float time;
+    Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        originalPos = gameObject.transform.position;
+        startRotation = transform.rotation;
+    }
+
+    public void OnDisable()
+    {
+        transform.position = originalPos;
+        transform.rotation = Quaternion.Slerp(startRotation, Quaternion.identity, time);
+        time += Time.deltaTime;
+        rb.angularVelocity = Vector3.forward * 0f;
+        Debug.Log("aa");
+    }
+    void Teste()
+    {
+
+    }
+}
